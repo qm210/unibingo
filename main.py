ad0559705b5e6ca71b2edd0293f41e2732a4d93a
qm210 <quantenmace@gmail.com>
2020-04-24 13:09:43 +0200

    new mode because hänähänä

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 wordsFile = './words.txt'
 
 backgroundPngFile = './background.png'
-fontSize = 17
+fontSize = 16
 
 marginX = 80
 marginY = 90
@@ -14,42 +14,32 @@ marginY = 90
 clean = lambda line: stripLastComma(line.replace('\n','').strip()).replace(';','\n')
 stripLastComma = lambda line: line[0:-1] if line and line[-1] == ',' else line
 
+maxMode = True
+
 def readFile():
     handle = open(wordsFile, encoding='utf-8')
     words = [clean(line) for line in handle.readlines()]
     handle.close()
     return words
 
-def generateField(words):
-    wordCount = len(words)
-    fieldSize = int(sqrt(wordCount))
-    if fieldSize*fieldSize != wordCount:
-        print(f"line number in {wordsFile} must be a square number. Currently it is {wordCount}.")
-        quit()
-
-    field = []
-    for row in range(fieldSize):
-        fieldRow = []
-        for column in range(fieldSize):
-            fieldRow.append(words[row * fieldSize + column])
-        field.append(fieldRow)
-
-    return field, fieldSize, wordCount
-
-def drawCenteredText(draw, x, y, word, font, fill='white'):
+def drawCenteredText(draw, x, y, word, font, fill='white', lineDistance = 1.2):
     lines = word.split('\n')
-    lineDistance = 1.2 * font.size
     for l in range(len(lines)):
         line = lines[l].strip()
         w, h = draw.textsize(line, font=font)
         drawX = x - .5 * w
-        drawY = y + lineDistance * (l - .5 * (len(lines) + 1))
+        drawY = y + lineDistance * font.size * (l - .5 * (len(lines) + 1))
         draw.text((drawX, drawY), line, font=font, fill=fill)
-        #draw.text((drawX+1, drawY), line, font=font, fill=fill)
 
-def drawField(words, field, SEED):
+def drawField(words, SEED):
     wordCount = len(words)
-    fieldSize = sqrt(wordCount)
+    fieldSize = int(sqrt(wordCount))
+    if maxMode and wordCount == 48:
+        wordCount = 49
+        fieldSize = 7
+    elif fieldSize * fieldSize != wordCount:
+        print(f"line number in {wordsFile} must be a square number. Currently it is {wordCount}.")
+        quit()
 
     image = Image.open(backgroundPngFile)
     imageWidth, imageHeight = image.size
@@ -60,28 +50,39 @@ def drawField(words, field, SEED):
     wordColor = {word: f'hsl({random.randint(0, 360)}, 100%, 70%)' for word in words}
 
     random.seed(666 * SEED)
-    shuffledWords = random.sample(words, wordCount)
+    shuffledWords = random.sample(words, len(words))
 
     distanceX = (imageWidth - 2*marginX) / (fieldSize - 1)
     distanceY = (imageHeight - 2*marginY) / (fieldSize - 1)
+
+    if maxMode:
+        maxWord = 'Max ein\n"XW<3"\nschicken'
+        shuffledWords = shuffledWords[0:24] + [maxWord] + shuffledWords[24:48]
+        wordColor.update({maxWord: 'rgb(255,80,255)'})
+
+        xw = Image.open('./xw.png')
+        xwW, xwH = xw.size
+        image.paste(xw, (round((imageWidth-xwW)/2), round((imageHeight-xwH)/2) + 7), mask=xw)
 
     for index in range(wordCount):
         posX = marginX + distanceX * (index % fieldSize)
         posY = marginY + distanceY * (int(index / fieldSize))
         word = shuffledWords[index]
-        drawCenteredText(draw, posX, posY, word, font, fill = wordColor[word])
+        if maxMode and index == 24:
+            drawCenteredText(draw, posX, posY - 20, word, font, fill = wordColor[word], lineDistance=1.1)
+        else:
+            drawCenteredText(draw, posX, posY, word, font, fill = wordColor[word])
 
-    draw.text((imageWidth - 210 - 10, imageHeight - .1 * 210 - 10), 'QM says hi (super-soberly)', fill='rgba(255,255,255,128)', font=font)
+    draw.text((imageWidth - .4*210 - 30, imageHeight - .1 * 210 - 10), 'QM says hi v2', fill='rgba(255,255,255,88)', font=font)
 
-    fileName = f'./final{SEED}.png'
+    fileName = f'./unibingo{SEED}.png'
     image.save(fileName, 'PNG')
     return fileName
 
 if __name__ == '__main__':
     SEEDS = [int(arg) for arg in sys.argv[1:]] if len(sys.argv) > 1 else [0]
     words = readFile()
-    field = generateField(words)
     for seed in SEEDS:
-        fileName = drawField(words, field, seed)
+        fileName = drawField(words, seed)
         print(f'written: {fileName}')
     print('done.')
